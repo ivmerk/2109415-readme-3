bad9c00ec31ba8705de9608eaf24793cabe4514f
ivmerk <ivan.e.merkulov@gmail.com>
2023-03-28 21:21:16 +0300

    2.9.33. Детализирует ответы обработчиков маршрутов (@ApiResponse). С помощью декоратора `@ApiResponse` можно детализировать ответ: добавить описание, код состояния и так далее.

--- project/apps/users/src/app/authentication/authentication.controller.ts
+++ project/apps/users/src/app/authentication/authentication.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { UserRdo } from './rdo/user.rdo';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -14,6 +14,10 @@ export class AuthenticationController {
     private readonly authService: AuthenticationService
   ) {}
 
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'The new user has been successfully created.'
+  })
   @Post('register')
   public async create(@Body() dto: CreateUserDto) {
     const newUser = await this.authService.register(dto);
@@ -22,8 +26,15 @@ export class AuthenticationController {
 
   @ApiResponse({
     type: LoggedUserRdo,
+    status: HttpStatus.OK,
+    description: 'User has been successfully logged.'
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'Password or Login is wrong.',
   })
   @Post('login')
+  @HttpCode(HttpStatus.OK)
   public async login(@Body() dto: LoginUserDto) {
     const verifiedUser = await this.authService.verifyUser(dto);
     return fillObject(LoggedUserRdo, verifiedUser);
@@ -31,6 +42,8 @@ export class AuthenticationController {
 
   @ApiResponse({
     type: UserRdo,
+    status: HttpStatus.OK,
+    description: 'User found'
   })
   @Get(':id')
   public async show(@Param('id') id: string) {
