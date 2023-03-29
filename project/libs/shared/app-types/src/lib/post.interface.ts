852c1fe9832611ab0271490a9c54356899a199e9
ivmerk <ivan.e.merkulov@gmail.com>
2023-03-29 19:29:16 +0300

    разбил интерфейс постов по видам

--- project/libs/shared/app-types/src/lib/post.interface.ts
+++ project/libs/shared/app-types/src/lib/post.interface.ts
@@ -5,3 +5,28 @@ export interface Post {
   tag?: string[];
   type: PostType;
 }
+
+export interface VideoPost extends Post {
+  name: string,
+  video: string,
+}
+
+export interface TextPost extends Post {
+  name: string,
+  announcement: string,
+  text: string,
+}
+
+export interface QuotePost extends Post {
+  name: string,
+  autor: string,
+}
+
+export interface PicturePost extends Post {
+  picture: string,
+}
+
+export interface LinkPost extends Post {
+  link: string,
+  options?: string,
+}
