b55a195c3aa087778a385060398f09cc5b231cab
ivmerk <ivan.e.merkulov@gmail.com>
2023-04-01 20:35:32 +0300

    поправил BlogUserModule не раоботает getUserById где-то в сервисе ошибка

--- project/apps/users/src/app/blog-user/blog-user.module.ts
+++ project/apps/users/src/app/blog-user/blog-user.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
-import { BlogUserMemoryRepository } from './blog-user-memory.repository';
 import { BlogUserModel, BlogUserSchema } from './blog-user.model';
 import { MongooseModule } from '@nestjs/mongoose';
+import { BlogUserRepository } from './blog-user.repository';
 
 @Module({
   imports: [MongooseModule.forFeature([
     { name: BlogUserModel.name, schema: BlogUserSchema }
   ])],
-  providers: [BlogUserMemoryRepository],
-  exports: [BlogUserMemoryRepository]
+  providers: [BlogUserRepository],
+  exports: [BlogUserRepository]
 })
 
 export class BlogUserModule {}
