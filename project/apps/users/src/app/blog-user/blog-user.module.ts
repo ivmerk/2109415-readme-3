4614b174ddc3f100f43665da5b6510f123ab7403
ivmerk <ivan.e.merkulov@gmail.com>
2023-04-01 20:02:47 +0300

    3.9.20. Подключит MongooseModule в модуль BlogUserModule. Предпоследним шагом подключим `MongooseModule` к модулю `BlogUserModule` и заменим `BlogUserMemoryRepository` на `BlogUserRepository`.

--- project/apps/users/src/app/blog-user/blog-user.module.ts
+++ project/apps/users/src/app/blog-user/blog-user.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { BlogUserMemoryRepository } from './blog-user-memory.repository';
+import { BlogUserModel, BlogUserSchema } from './blog-user.model';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [MongooseModule.forFeature([
+    { name: BlogUserModel.name, schema: BlogUserSchema }
+  ])],
   providers: [BlogUserMemoryRepository],
   exports: [BlogUserMemoryRepository]
 })
