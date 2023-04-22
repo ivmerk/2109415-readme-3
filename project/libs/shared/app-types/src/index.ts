13f481795c5dcaa492b0f5ad12a94c9ce6fc8511
ivmerk <ivan.e.merkulov@gmail.com>
2023-04-22 15:28:04 +0300

    5.4.15. Добавит сохранение информации о загруженных файлах в базу данных. Теперь опишем модель, схему, интерфейс и Entity для файлов. Информацию о загруженных файлах будем сохранять в базе данных.
    Затем доработаем контроллер `FileController`, добавив обработчик `show`.
    Он будет отвечать за извлечение информации о файле из БД.

--- project/libs/shared/app-types/src/index.ts
+++ project/libs/shared/app-types/src/index.ts
@@ -4,3 +4,4 @@ export * from './lib/comment.interface';
 export * from './lib/favorite.interface';
 export * from './lib/post.interface';
 export * from './lib/post-type.enum';
+export * from './lib/file.interface';
