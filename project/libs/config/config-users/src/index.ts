953cfce1165460eee5724d6459d569fb8fc1074a
ivmerk <ivan.e.merkulov@gmail.com>
2023-04-24 10:06:16 +0300

    5.4.26. Добавит настройки для подготовки AccessToken. Обновим библиотеку `config/config-user` и добавим поддержку настроек, необходимых для подготовки токенов. Настройки, которые будем передавать через переменные окружения потребуется две: время жизни токена и секретная фраза.
    Создадим модули `jwt.config.ts` и опишем новое пространство настроек `jwt`.

--- project/libs/config/config-users/src/index.ts
+++ project/libs/config/config-users/src/index.ts
@@ -1,4 +1,5 @@
-export * from './lib/config-config-users.module';
+export * from './lib/config-users.module';
 export { default as dbConfig } from './lib/config/db.config';
 export { default as appConfig } from './lib/config/app.config';
+export { default as jwtConfig } from './lib/config/jwt.config';
 export * from './lib/get-mongoose-options';
