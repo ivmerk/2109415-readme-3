7d4095464a55ec9d7ab59749bb4e37afb9ede9d4
ivmerk <ivan.e.merkulov@gmail.com>
2023-04-24 10:20:16 +0300

    5.4.30. Подключит JwtModule в AuthenticationModule. Чтобы начать выписывать токены, необходимо подключить `JwtModule` в модуль `Authentication`. Для инициализации `JwtModule` воспользуемся методом `registerAsync`. В объекте передадим информацию о зависимостях, которые должны быть доступны в `useFactory`. Нам потребуются только сервис конфигурации.
    В контроллере `AuthenticationController` добавим в метод `login` действие для
    формирования `AccessToken`.

--- project/apps/users/src/app/authentication/authentication.module.ts
+++ project/apps/users/src/app/authentication/authentication.module.ts
@@ -2,10 +2,21 @@ import { Module } from '@nestjs/common';
 import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
 import { BlogUserModule } from '../blog-user/blog-user.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { getJwtOptions } from '@project/config/config-users';
 
 @Module({
-  imports: [BlogUserModule],
+  imports: [
+    BlogUserModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: getJwtOptions
+    })
+  ],
   controllers: [AuthenticationController],
-  providers: [AuthenticationService],
+  providers: [
+    AuthenticationService,
+  ],
 })
 export class AuthenticationModule {}
