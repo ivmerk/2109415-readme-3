2a08cfbd8b54e7c8c399d2aeccb6c1c281aeb00e
ivmerk <ivan.e.merkulov@gmail.com>
2023-04-25 19:51:49 +0300

    6.5.9. Добавит перечисление RabbitRouting. Routing Key - это строка, которая используется для определения, какие очереди в RabbitMQ получат определённые сообщения. Когда producer отправляет сообщение в exchange, он также указывает `Routing Key`. Этот ключ связывает сообщение с определённой очередью.
    Обменник использует Routing Key для определения, какие очереди нужно отправить
    сообщение. Если Routing Key совпадает с Routing Key, связанным с очередью,
    сообщение отправляется в эту очередь.
    
    Routing Key может быть любой строкой и может содержать произвольные символы,
    включая точки и слэши, которые могут быть использованы для организации
    Routing Key в иерархическую структуру.
    
    Заведём отдельное перечисление `RabbitRouting`. В нём определим все
    возможные RoutingKey.

--- project/libs/shared/app-types/src/index.ts
+++ project/libs/shared/app-types/src/index.ts
@@ -7,3 +7,4 @@ export * from './lib/post-type.enum';
 export * from './lib/file.interface';
 export * from './lib/token-payload.interface';
 export * from './lib/subscriber.interface';
+export * from './lib/rabbit-routing.enum';
