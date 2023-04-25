23f3c1b91567984c816934dc0b284a512384aa68
ivmerk <ivan.e.merkulov@gmail.com>
2023-04-25 20:22:43 +0300

    fixing

--- project/apps/notify/src/app/email-subscriber/email-subscriber.controller.ts
+++ project/apps/notify/src/app/email-subscriber/email-subscriber.controller.ts
@@ -11,9 +11,9 @@ export class EmailSubscriberController {
   ) {}
 
   @RabbitSubscribe({
-    exchange: 'typoteka.notify',
+    exchange: 'readme.notify',
     routingKey: RabbitRouting.AddSubscriber,
-    queue: 'typoteka.notify',
+    queue: 'readme.notify',
   })
   public async create(subscriber: CreateSubscriberDto) {
     this.subscriberService.addSubscriber(subscriber);
