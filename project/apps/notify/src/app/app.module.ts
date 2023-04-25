f11961b8035a69a177102047db0349cff93e03ff
ivmerk <ivan.e.merkulov@gmail.com>
2023-04-25 20:15:40 +0300

    6.5.10. Подготовит основу для модуля EmailSubscriber. WIP. Логику по сохранению подписчика в базе данных MongoDB опишем в отдельном модуле `EmailSubscriber`. Похожую процедуру проделывали уже неоднократно, поэтому сильно заострять внимание не станем.
    WIP: Пока не создан `EmailSubscriberModule` и `EmailSubscriberController`.

--- project/apps/notify/src/app/app.module.ts
+++ project/apps/notify/src/app/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigNotifyModule } from '@project/config/config-notify';
+import { getMongooseOptions } from '@project/util/util-core';
+import { EmailSubscriberModule } from './email-subscriber/email-subscriber.module';
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigNotifyModule,
+    MongooseModule.forRootAsync(getMongooseOptions('application.db')),
+    EmailSubscriberModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
