f1797fd2230d71f8c415d1ef912cdf02fb5a1b3e
ivmerk <ivan.e.merkulov@gmail.com>
2023-04-25 11:33:33 +0300

    6.5.5. Добавит новый общий интерфейс Subscriber. Нам потребуется интерфейс для описания подписчиков. Объявим его в библиотеке `shared/app-types`. В интерфейс предусмотрим поля, которые потребуются для решения задачи отправки уведомлений.

--- project/libs/shared/app-types/src/index.ts
+++ project/libs/shared/app-types/src/index.ts
@@ -6,3 +6,4 @@ export * from './lib/post.interface';
 export * from './lib/post-type.enum';
 export * from './lib/file.interface';
 export * from './lib/token-payload.interface';
+export * from './lib/subscriber.interface';
