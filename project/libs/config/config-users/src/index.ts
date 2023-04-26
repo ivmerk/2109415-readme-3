6e2dbaf90c355c0bec986499987d25d4b3d56fe2
ivmerk <ivan.e.merkulov@gmail.com>
2023-04-26 09:17:28 +0300

    6.5.12. Добавит модуль Notify в сервис users. Чтобы полноценно протестировать добавление подписчиков, добавим в сервис `users` возможность взаимодействовать с RabbitMQ. Для этого обновим конфигурацию приложения и добавим новый модуль `Notify`.
    Отправку сообщений в RabbitMQ сделаем в сервисе `Notify`.

--- project/libs/config/config-users/src/index.ts
+++ project/libs/config/config-users/src/index.ts
@@ -4,3 +4,4 @@ export { default as appConfig } from './lib/config/app.config';
 export { default as jwtConfig } from './lib/config/jwt.config';
 export * from './lib/get-mongoose-options';
 export * from './lib/get-jwt-options';
+export { default as rabbitConfig } from './lib/config/rabbit.config';
