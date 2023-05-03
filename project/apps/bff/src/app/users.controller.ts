e139d7d52e6034ac1fd4c32b3ddf6ff5ab7cebfd
ivmerk <ivan.e.merkulov@gmail.com>
2023-05-03 12:18:36 +0300

    7.5.21. Добавит пример проксирования запросов login и refreshToken. Определим в контроллере `UsersController` обработчики нашего API. В теле обработчика напишем код для проксирования запросов к сервису `Users`.

--- project/apps/bff/src/app/users.controller.ts
+++ project/apps/bff/src/app/users.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { Body, Controller, Post } from '@nestjs/common';
+import { LoginUserDto } from './dto/login-user.dto';
+import { ApplicationServiceURL } from './app.config';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(
+    private readonly httpService: HttpService
+  ) {}
+
+  @Post('login')
+  public async login(@Body() loginUserDto: LoginUserDto) {
+    const { data } = await this.httpService.axiosRef.post(`${ApplicationServiceURL.Users}/login`, loginUserDto);
+    return data;
+  }
+
+  @Post('refresh')
+  public async refreshToken(@Req() req: Request) {
+    const { data } = await this.httpService.axiosRef.post(`${ApplicationServiceURL.Users}/refresh`, null, {
+      headers: {
+        'Authorization': req.headers['authorization']
+      }
+    });
+
+    return data;
+  }
+}
