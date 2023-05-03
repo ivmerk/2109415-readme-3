9dfd675571fba8a00daa062966194695455608df
ivmerk <ivan.e.merkulov@gmail.com>
2023-05-03 12:04:41 +0300

    7.5.17.Добавит заготовки модулей для API.

--- project/apps/bff/src/app/app.module.ts
+++ project/apps/bff/src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { BlogController } from './blog.controller';
+import { UsersController } from './users.controller';
 
 
 @Module({
   imports: [],
   controllers: [],
-  providers: [],
+  providers: [
+    UsersController,
+    BlogController,
+  ],
 })
 export class AppModule {}
